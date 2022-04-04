575cd897ce24ae11a3837bf3267e093561e0453b
marioEzquerro <marioezquerroruiz@gmail.com>
2022-04-04 15:04:09 +0200

    Cositas

--- IAPPT.py
+++ IAPPT.py
@@ -1,28 +1,26 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
 
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-
-
+mp_drawing = mp.solutions.drawing_utils # 
+mp_drawing_styles = mp.solutions.drawing_styles # estilos del formato
+mp_hands = mp.solutions.hands # modelo usado
 
 # For webcam input:
-cap = cv2.VideoCapture(0)
+camara = cv2.VideoCapture(0)
+
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    model_complexity = 0,
+    max_num_hands = 1,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5) as hands:
 
-  while cap.isOpened():
-    success, image = cap.read()
+  while camara.isOpened():
+    success, image = camara.read()
     if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      break
+      print("Camara no detectada")
+      continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    # To improve performance, optionally mark the image as not writeable to pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
@@ -40,10 +38,12 @@ with mp_hands.Hands(
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
 
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+
+
+    # Voltear la imagen horizontalmente y mostrarla
+    cv2.imshow('IAPPT', cv2.flip(image, 1))
     
-    # Stop program pressing Esc
+    # Detener programa al pulsar 'Esc'
     if cv2.waitKey(5) & 0xFF == 27:
       break
-cap.release()
+camara.release()
