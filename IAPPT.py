d9d206bcefd0040b22f41f113553cac710bcf35e
marioEzquerro <marioezquerroruiz@gmail.com>
2022-04-06 12:32:55 +0200

    Reconocimento eje y volteado

--- IAPPT.py
+++ IAPPT.py
@@ -3,7 +3,6 @@ import mediapipe as mp
 import drawing_styles as ds
 
 mp_drawing = mp.solutions.drawing_utils
-# mp_drawing_styles = ds.  # estilos del formato
 mp_hands = mp.solutions.hands  # seleccionar el modelo que usaremos
 
 # Entrada de video por webcam:
@@ -11,13 +10,17 @@ camara = cv2.VideoCapture(0)
 
 # Estas funciones determinan si el gesto 
 # toma como parametro las posiciones las llemas de los dedos y los nudillos
-def gestoEsPiedra(indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
-    if ((indxp < indxt) and (corzp < corzt) and (anlp < anlt) and (meñp < meñt)):
+def gestoEsPiedra(muñeca ,indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
+    if ((muñeca > corzp) and (indxp < indxt) and (corzp < corzt) and (anlp < anlt) and (meñp < meñt)):
+        return True
+    elif ((muñeca < corzp) and (indxp > indxt) and (corzp > corzt) and (anlp > anlt) and (meñp > meñt)):
         return True
     return False
 
-def gestoEsPapel(indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
-    if ((indxp > indxt) and (corzp > corzt) and (anlp > anlt) and (meñp > meñt)):
+def gestoEsPapel(muñeca, indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
+    if ((muñeca > corzp) and (indxp > indxt) and (corzp > corzt) and (anlp > anlt) and (meñp > meñt)):
+        return True
+    elif ((muñeca < corzp) and (indxp < indxt) and (corzp < corzt) and (anlp < anlt) and (meñp < meñt)):
         return True
     return False
 
@@ -28,9 +31,10 @@ def gestoEsTijera(indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
 
 
 with mp_hands.Hands(
-        model_complexity = 0,
+        static_image_mode = False,
+        model_complexity = 1,
         max_num_hands = 1,
-        min_detection_confidence = 0.5,
+        min_detection_confidence = 0.6,
         min_tracking_confidence = 0.5) as hands:
 
     while camara.isOpened():
@@ -50,9 +54,10 @@ with mp_hands.Hands(
 
 
         if results.multi_hand_landmarks:
-             # usamos solo el 1º elemento encontrado ya que solo trabajaremos con una mano a la vez
+            # usamos solo el 1º elemento encontrado ya que solo trabajaremos con una mano a la vez
             mano = results.multi_hand_landmarks[0]
             # TODO explicar esto en doc
+            muñeca = mano.landmark[0].y
             indxp = mano.landmark[6].y  
             indxt = mano.landmark[8].y  
             corzp = mano.landmark[10].y  
@@ -63,15 +68,13 @@ with mp_hands.Hands(
             meñt = mano.landmark[20].y  
             txt = ''
 
-            if (gestoEsPiedra(indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt)):
+            if (gestoEsPiedra(muñeca, indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt)):
                 txt = 'Piedra'
-            if (gestoEsPapel(indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt)):
+            if (gestoEsPapel(muñeca, indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt)):
                 txt = 'Papel'
             if (gestoEsTijera(indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt)):
                 txt = 'Tijera'
-              
-            #cv2.putText(image, txt, (200,200), cv2.FONT_HERSHEY_DUPLEX, 2, (0,0,0), 2)
-            
+                    
             # Mostrar la imagen vista por la camara
             mp_drawing.draw_landmarks(
                 image,
@@ -80,7 +83,7 @@ with mp_hands.Hands(
                 ds.get_hand_landmarks_style(txt),
                 ds.get_hand_connections_style(txt)
             )
-            cv2.imshow('IAPPT', image)
+            cv2.imshow('IAPPT', cv2.flip(image,1))
 
 
         # Detener programa al pulsar 'Esc'
