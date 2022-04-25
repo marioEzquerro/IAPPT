85b225625b24142b87989f8160501fa86d6eb6c9
marioEzquerro <marioezquerroruiz@gmail.com>
2022-04-25 10:10:34 +0200

    Intento de integracion no funcional 1

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from matplotlib.cbook import print_cycles
+import tensorflow as tf
 import random
 import cv2
 import mediapipe as mp
@@ -5,9 +7,29 @@ import drawing_styles as ds
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands  # seleccionar el modelo que usaremos
+feature_columns = ['muneca_Y','muneca_X','indicep_Y','indicep_X','indicet_Y','indicet_X','corazonp_Y','corazonp_X','corazont_Y','corazont_X','anularp_Y','anularp_X','anulart_Y','anulart_X','meniquep_Y','meniquep_X','meniquet_Y','meniquet_X']
+
+# model = tf.estimator.DNNClassifier(
+#     feature_columns=feature_columns,
+#     hidden_units=[32, 10],
+#     n_classes=3,
+#     model_dir='model'
+# )
+
+# def input_fn(features, labels, training=True, batch_size=128):
+#   # Convertir los inputs a dataset
+#   dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+#   # Mezclar los datos
+#   if training:
+#       dataset = dataset.shuffle(1000).repeat()
+#   return dataset.batch(batch_size)
+
+
+
 
 # Entrada de video por webcam:
-camara = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
 # Estas funciones determinan si el gesto 
 # toma como parametro las posiciones las llemas de los dedos y los nudillos
@@ -28,7 +50,7 @@ def gestoEsPapel(muñeca, indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
 def gestoEsTijera(muñeca, indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
     if ((muñeca > corzp) and (indxp > indxt) and (corzp > corzt) and (anlp < anlt) and (meñp < meñt)):
         return True
-    if ((muñeca < corzp) and (indxp < indxt) and (corzp < corzt) and (anlp > anlt) and (meñp > meñt)):
+    elif ((muñeca < corzp) and (indxp < indxt) and (corzp < corzt) and (anlp > anlt) and (meñp > meñt)):
         return True
     return False
 
@@ -58,8 +80,8 @@ with mp_hands.Hands(
         min_detection_confidence = 0.6,
         min_tracking_confidence = 0.5) as hands:
 
-    while camara.isOpened():
-        success, img = camara.read()
+    while camera.isOpened():
+        success, img = camera.read()
 
         if not success:
             print("Camara no detectada")
@@ -72,19 +94,33 @@ with mp_hands.Hands(
 
         if results.multi_hand_landmarks:
             # usamos solo el 1º elemento encontrado ya que solo trabajaremos con una mano a la vez
-            mano = results.multi_hand_landmarks[0]
+            hand = results.multi_hand_landmarks[0]
             # TODO explicar esto en doc
-            muñeca = mano.landmark[0].y
-            indxp = mano.landmark[6].y  
-            indxt = mano.landmark[8].y  
-            corzp = mano.landmark[10].y  
-            corzt = mano.landmark[12].y  
-            anlp = mano.landmark[14].y  
-            anlt = mano.landmark[16].y  
-            meñp = mano.landmark[18].y  
-            meñt = mano.landmark[20].y  
+            muñeca = hand.landmark[0].y
+            indxp = hand.landmark[6].y  
+            indxt = hand.landmark[8].y  
+            corzp = hand.landmark[10].y  
+            corzt = hand.landmark[12].y  
+            anlp = hand.landmark[14].y  
+            anlt = hand.landmark[16].y  
+            meñp = hand.landmark[18].y  
+            meñt = hand.landmark[20].y  
             txt = ''
             global accion
+
+            print(hand.landmark[0].y, hand.landmark[0].x, 
+                hand.landmark[6].y, hand.landmark[6].x,
+                hand.landmark[8].y, hand.landmark[8].x,
+                hand.landmark[10].y, hand.landmark[10].x,
+                hand.landmark[12].y, hand.landmark[12].x,
+                hand.landmark[14].y, hand.landmark[14].x,
+                hand.landmark[16].y, hand.landmark[16].x,
+                hand.landmark[18].y, hand.landmark[18].x,
+                hand.landmark[20].y, hand.landmark[20].x
+            )
+            # camera.release()
+            # cv2.destroyAllWindows()
+            
 
             if gestoEsPiedra(muñeca, indxp, indxt, corzp, corzt, anlp, anlt, meñp, meñt):
                 accion = 1
@@ -96,7 +132,7 @@ with mp_hands.Hands(
             # Mostrar la imagen vista por la camara
             mp_drawing.draw_landmarks(
                 img,
-                mano,
+                hand,
                 mp_hands.HAND_CONNECTIONS,
                 ds.get_hand_landmarks_style(accion),
                 ds.get_hand_connections_style(accion)
@@ -111,6 +147,6 @@ with mp_hands.Hands(
         
         cv2.imshow('IAPPT', cv2.flip(img,1))
 
-camara.release()
+camera.release()
 cv2.destroyAllWindows()
 
